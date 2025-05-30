24f8b88a39c55b5baa958530288b590e417f157a
loftyyyy <lofty08.business@gmail.com>
2025-05-31 00:10:56 +0800

    Node with generics is very nice

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,15 @@
+public class DoublyLinkedList{
+    public int length;
+    private Node<T> head; 
+
+    class DoubyLinkedList(){
+        this.length = 0;
+        this.head = null;
+    }
+
+    public void prepend(){
+
+    }
+
+
+}
